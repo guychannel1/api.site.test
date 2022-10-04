0c959b135265c7b2469f0bf7a96ee58b1358c4b0
guyssar <69724220+guychannel1@users.noreply.github.com>
2022-10-04 13:59:07 +0700

    Create Json API

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState,useEffect } from "react";
+port : 4000
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
